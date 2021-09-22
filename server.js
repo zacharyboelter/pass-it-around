ee141d5516fd7fe0ccb7a48acbba8b16ad95fe05
Zachary Boelter <zboelter@gmail.com>
2021-09-22 06:45:09 -0500

    final final

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     <html>
         <body>
             <h2>99 Bottles of beer on the wall...</h2> <br
-            <h3><a href="http://localhost:3000/98">Take one down, pass it around</a><h3>
+            <h3><a href="/98">Take one down, pass it around</a><h3>
         </body>
     </html>`);
 });
@@ -26,16 +26,16 @@ app.get('/:number_of_bottles', (req, res) => {
     <html>
         <body>
             <h2>${req.params.number_of_bottles} Bottles of beer on the wall...</h2> <br
-            <h3><a href="http://localhost:3000/${req.params.number_of_bottles - 1}">Take one down, pass it around</a><h3>
+            <h3><a href="/${req.params.number_of_bottles - 1}">Take one down, pass it around</a><h3>
         </body>
     </html>`);
-//////////// phase 3
+        //////////// phase 3
     } else
         res.send(`
     <html>
         <body>
             <h2>Beer run...</h2> <br
-            <h3><a href="http://localhost:3000/99">Nose goes!</a><h3>
+            <h3><a href="/99">Nose goes!</a><h3>
         </body>
     </html>`);
 });
